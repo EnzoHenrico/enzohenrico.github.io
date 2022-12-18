4f5d96ba3b7643d047c8e62ed737be13884d3276
EnzoHenrico <enzo.mascia1@gmail.com>
2022-12-18 17:44:23 -0300

    added page interactions and animation logic, improved initial states

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,7 +27,11 @@ function deviceType() {
 
 let inputType = deviceType()
 
-// Action functions
+// Selectors
+const toolbarIcons = document.querySelectorAll(".toolbar .icons-section ul li")
+const appWindows = document.querySelectorAll(".basic-window")
+
+// Declare actions and animations 
 const moveWindows = (appWindow) => {        
     const titleBar = appWindow.querySelector(".title-bar")
     
@@ -55,7 +59,6 @@ const moveWindows = (appWindow) => {
             initialX = newX
             initialY = newY
         }
-        
     })
 
     // End movement
@@ -69,13 +72,36 @@ const titleBarActions = (appWindow) => {
 
     const minimizeButton = appWindow.querySelector(".title-bar .buttons-box .minimize")
     const closeButton = appWindow.querySelector(".title-bar .buttons-box .close")
+    let toolbarIconStatus
+
+    toolbarIcons.forEach((icon) => { 
+        if (appWindow.dataset.name === icon.dataset.appname) toolbarIconStatus = icon.children[1]
+    })
     
-    minimizeButton.addEventListener(events[inputType].down, () => appWindow.classList.add("hidden"))
-    closeButton.addEventListener(events[inputType].down, () => appWindow.classList.add("closed"))
+    // Minimize animation
+    minimizeButton.addEventListener(events[inputType].down, () => {
+        appWindow.classList.add("minimize-animation")
+        toolbarIconStatus["id"] = "minimized"
+
+        setTimeout(() => {
+            appWindow.classList.remove("minimize-animation")
+            appWindow.classList.add("hidden")
+        }, 500)
+    })
+
+    // Close animation
+    closeButton.addEventListener(events[inputType].down, () => {
+        appWindow.classList.add("close-animation")
+        toolbarIconStatus["id"] = "closed"
+
+        setTimeout(() => {
+            appWindow.classList.remove("close-animation")
+            appWindow.classList.add("closed")
+        }, 500)
+    })
 }
 
 // Set windows movement and action
-const appWindows = document.querySelectorAll(".basic-window")
 appWindows.forEach(
     (appWindow) => {
         // Move elements
@@ -88,18 +114,50 @@ appWindows.forEach(
 
 
 // Set toolbar actions
-const toolbarIcons = document.querySelectorAll(".toolbar .icons-section ul li")
 toolbarIcons.forEach(
     (icon) => {
         let relativeWindow
-        
+        let toolbarIconStatus = icon.children[1]
+
         appWindows.forEach((appWindow) => { 
             if (appWindow.dataset.name === icon.dataset.appname) relativeWindow = appWindow 
         })
         
+        // Show/Open states and animations 
         icon.addEventListener(events[inputType].down, () => { 
-            if (relativeWindow.classList.contains("hidden")) relativeWindow.classList.remove("hidden")
-            if (relativeWindow.classList.contains("closed")) relativeWindow.classList.remove("closed")
+            // When minimized
+            if (relativeWindow.classList.contains("hidden")) {
+                
+                relativeWindow.classList.remove("hidden")
+                toolbarIconStatus["id"] = "openned"
+
+                relativeWindow.classList.add("maximize-animation")
+                setTimeout(() => {
+                    relativeWindow.classList.remove("maximize-animation")
+                }, 1000)
+            }
+
+            // When closed
+            else if (relativeWindow.classList.contains("closed")) {
+                relativeWindow.classList.remove("closed")
+                relativeWindow.classList.add("open-animation")
+                toolbarIconStatus["id"] = "openned"
+
+                setTimeout(() => {
+                    relativeWindow.classList.remove("open-animation")
+                }, 1000)
+            }
+            
+            else {
+                // When opened
+                relativeWindow.classList.add("minimize-animation")
+                toolbarIconStatus["id"] = "minimized"
+
+                setTimeout(() => {
+                    relativeWindow.classList.remove("minimize-animation")
+                    relativeWindow.classList.add("hidden")
+                }, 500)
+            }
         })
     }
 );
